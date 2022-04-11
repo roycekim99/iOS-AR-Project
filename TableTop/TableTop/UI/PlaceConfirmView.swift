901fcfefe1eedf3471b832bafa3db61820715c80
Ash-Ley-L <lili062415@gmail.com>
2022-04-10 19:18:21 -0700

    same as alpha version. still need to update

--- TableTop/TableTop/UI/PlaceConfirmView.swift
+++ TableTop/TableTop/UI/PlaceConfirmView.swift
@@ -5,4 +5,52 @@
 //  Created by Royce Kim on 4/7/22.
 //
 
-import Foundation
+import SwiftUI
+
+// TODO: update
+
+struct PlaceConfirmView: View {
+//    @EnvironmentObject var placementSettings: placementSettings
+        
+        // View to place object
+        var body: some View {
+            HStack {
+                Spacer()
+                
+                PlacementButton(systemIconName: "xmark.circle.fill") {
+                    print("Cancel Placement Button pressed.")
+//                    self.placementSettings.selectedModel = nil
+                }
+                
+                Spacer()
+                
+                PlacementButton(systemIconName: "checkmark.circle.fill") {
+                    print("Confirm Placement button pressed.")
+                    
+//                    self.placementSettings.confirmedModel = self.placementSettings.selectedModel
+//
+//                    self.placementSettings.selectedModel = nil
+                }
+                Spacer()
+            }
+            .padding(.bottom, 30)
+        }
+    }
+
+    struct PlacementButton: View {
+        let systemIconName: String
+        let action: () -> Void
+        
+        var body: some View {
+            Button(action: {
+                self.action()
+            }) {
+                Image(systemName: systemIconName)
+                    .font(.system(size:50, weight: .light, design: .default))
+                    .foregroundColor(.white)
+                    .buttonStyle(PlainButtonStyle())
+            }
+            .frame(width: 75, height: 75)
+        }
+}
+
