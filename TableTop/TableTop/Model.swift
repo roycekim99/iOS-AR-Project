3f3512deee8c745b7bc9c87cd2ad4fc0b8839187
Ash-Ley-L <lili062415@gmail.com>
2022-04-11 14:40:47 -0700

    Model struct

--- TableTop/TableTop/Model.swift
+++ TableTop/TableTop/Model.swift
@@ -5,4 +5,28 @@
 //  Created by Royce Kim on 4/7/22.
 //
 
-import Foundation
+import SwiftUI
+import RealityKit
+import Combine
+
+// TODO: uncomment position after figuring out how to get position
+class Model {
+    var name: String
+    var category: ModelCategory
+    var thumbnail: UIImage
+    var childs: [Model]
+    var scaleCompensation: Float
+    var assetID: Int
+//    var position: Float
+    
+    
+    init(name: String, category: ModelCategory, scaleCompensation: Float = 1.0, childs: [Model], assetID: Int) {
+        self.name = name
+        self.category = category
+        self.thumbnail = UIImage(named: name) ?? UIImage(systemName: "photo")!
+        self.scaleCompensation = scaleCompensation
+        self.childs = childs
+        self.assetID = assetID
+//        self.position = position
+    }
+}
