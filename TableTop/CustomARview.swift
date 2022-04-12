b4e3f41a0348f33ef32ed1f833a7d2217782cb52
Ash-Ley-L <lili062415@gmail.com>
2022-04-12 16:32:49 -0700

    ARView instance with focus entity

---
+++ TableTop/CustomARview.swift
@@ -0,0 +1,36 @@
+//
+//  CustomARview.swift
+//  TableTop
+//
+//  Created by Ashley Li on 4/12/22.
+//
+
+import RealityKit
+import ARKit
+import FocusEntity
+import SwiftUI
+
+// an ARview instance
+class CustomARView: ARView {
+    
+    var focusEntity: FocusEntity?
+    
+    required init(frame frameRect: CGRect) {
+        super.init(frame: frameRect)
+        
+        focusEntity = FocusEntity(on: self, focus: .classic)
+        
+        configure()
+    }
+    
+    @MainActor @objc required dynamic init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        session.run(config)
+    }
+    
+}
---
+++ TableTop/TableTop/CustomARView.swift
@@ -0,0 +1,40 @@
+//
+//  CustomARView.swift
+//  TableTop
+//
+//  Created by Ashley Li on 4/11/22.
+//
+
+// an ARView instance
+
+//import RealityKit
+//import ARKit
+//import FocusEntity
+//import SwiftUI
+//
+//class CustomARView: ARView {
+//    
+//    var focusEntity: FocusEntity?
+//    
+//    required init(frame frameRect: CGRect) {
+//        super.init(frame: frameRect)
+//        
+//        focusEntity = FocusEntity(on: self, focus: .classic)
+//        
+//        configure()
+//    }
+//    
+//    @MainActor @objc required dynamic init?(coder decoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    private func configure() {
+//        let config = ARWorldTrackingConfiguration()
+//        config.planeDetection = [.horizontal, .vertical]
+//        session.run(config)
+//    }
+//    
+//}
+
+
+
