74dbbbabd320f5a3d258b0c8beaa7a8dd2b72f6c
Ash-Ley-L <lili062415@gmail.com>
2022-04-12 20:28:28 -0700

    arView instance with focus entity

--- TableTop/CustomARview.swift
+++ TableTop/CustomARview.swift
@@ -3,7 +3,7 @@
 //  TableTop
 //
 //  Created by Ashley Li on 4/12/22.
-//
+
 
 import RealityKit
 import ARKit
@@ -12,25 +12,25 @@ import SwiftUI
 
 // an ARview instance
 class CustomARView: ARView {
-    
+
     var focusEntity: FocusEntity?
-    
+
     required init(frame frameRect: CGRect) {
         super.init(frame: frameRect)
-        
+
         focusEntity = FocusEntity(on: self, focus: .classic)
-        
+
         configure()
     }
-    
+
     @MainActor @objc required dynamic init?(coder decoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func configure() {
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = [.horizontal, .vertical]
         session.run(config)
     }
-    
+
 }
