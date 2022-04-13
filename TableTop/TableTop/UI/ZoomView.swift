afcd92bb66a6cd43c3180ae9474cae3b1d8db837
Ash-Ley-L <lili062415@gmail.com>
2022-04-12 21:06:55 -0700

    zoomView

--- TableTop/TableTop/UI/ZoomView.swift
+++ TableTop/TableTop/UI/ZoomView.swift
@@ -5,4 +5,25 @@
 //  Created by Royce Kim on 4/7/22.
 //
 
-import Foundation
+import SwiftUI
+import RealityKit
+import ARKit
+import Combine
+
+class ZoomView: ObservableObject {
+    @Published var ZoomEnabled: Bool = false {
+        willSet(newValue) {
+            print("Setting Zoom Enabled to \(String(describing: newValue.description))")
+        }
+    }
+    
+    @Published var changed: Bool = false {
+        willSet(newValue) {
+        }
+    }
+    
+    func getValue() -> Bool {
+        return ZoomEnabled
+    }
+}
+
