60e1c021ade652cbe48e51a5e8fab3f22e6efc48
Zaw Aung <jetaung@ucdavis.edu>
2022-04-12 20:35:51 -0700

    Added CustomARView into project

--- TableTop/TableTop/CustomARView.swift
+++ TableTop/TableTop/CustomARView.swift
@@ -7,34 +7,34 @@
 
 // an ARView instance
 
-//import RealityKit
-//import ARKit
-//import FocusEntity
-//import SwiftUI
-//
-//class CustomARView: ARView {
-//    
-//    var focusEntity: FocusEntity?
-//    
-//    required init(frame frameRect: CGRect) {
-//        super.init(frame: frameRect)
-//        
-//        focusEntity = FocusEntity(on: self, focus: .classic)
-//        
-//        configure()
-//    }
-//    
-//    @MainActor @objc required dynamic init?(coder decoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    private func configure() {
-//        let config = ARWorldTrackingConfiguration()
-//        config.planeDetection = [.horizontal, .vertical]
-//        session.run(config)
-//    }
-//    
-//}
+import RealityKit
+import ARKit
+import FocusEntity
+import SwiftUI
+
+class CustomARView: ARView {
+    
+    var focusEntity: FocusEntity?
+    
+    required init(frame frameRect: CGRect) {
+        super.init(frame: frameRect)
+        
+        focusEntity = FocusEntity(on: self, focus: .classic)
+        
+        configure()
+    }
+    
+    @MainActor @objc required dynamic init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        session.run(config)
+    }
+    
+}
 
 
 
